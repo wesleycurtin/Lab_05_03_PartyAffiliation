cf4d00a9d080c24716ef1fa009be8f7446c146ac
WesleyCurtin <curtinwl@mail.uc.edu>
2022-09-26 18:24:23 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String party = "Wes";
+
+        System.out.println("You chose " + party);
+
+        if (party == "D") {
+            System.out.println("You get a Democratic Donkey!");
+        } else if (party == "R") {
+            System.out.println("You get a Republican Elephant!");
+        } else if (party == "I") {
+            System.out.println("You get an Independent Person!");
+        } else {
+            System.out.println("You get other!");
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
